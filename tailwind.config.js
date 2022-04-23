2e1106dacb690f96260d6ba1b9fcfa415b1840c5
GeordiD <geordi.dosher@gmail.com>
2022-04-23 16:24:12 -0500

    Cover looks like figma

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ['src/**/*.astro'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'theme-light': '#D0B4AB',
+        'theme-med': '#AD7461',
+        'theme-dark': '#6D4B3F',
+        'theme-text': '#362621',
+      },
+      fontFamily: {
+        sans: 'Sora',
+      },
+    },
   },
   plugins: [],
 };
