e8e9df5c8f174973a29bb0e3d5caa3deffe5a9e2
GeordiD <geordi.dosher@gmail.com>
2022-04-24 17:20:01 -0500

    Cover animations, initial grid layout for about page

---
+++ src/components/CoverBackdrop.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { onMount } from 'svelte';
+  import anime from 'animejs';
+
+  onMount(() => {
+    anime({
+      targets: '.backdrop-line',
+      opacity: 0,
+      delay: anime.stagger(100),
+      translateY: 100,
+      duration: 1000,
+      direction: 'reverse',
+      easing: 'easeInQuart',
+    });
+  });
+</script>
+
+<div class="absolute bottom-0 w-full flex flex-col-reverse gap-8">
+  <div class="bg-theme-med w-full h-36 backdrop-line" />
+  <div class="bg-theme-med w-full h-24 backdrop-line" />
+  <div class="bg-theme-med w-full h-20 backdrop-line" />
+  <div class="bg-theme-med w-full h-16 backdrop-line" />
+  <div class="bg-theme-med w-full h-12 backdrop-line" />
+  <div class="bg-theme-med w-full h-8 backdrop-line" />
+  <div class="bg-theme-med w-full h-4 backdrop-line" />
+  <div class="bg-theme-med w-full h-2 backdrop-line" />
+  <div class="bg-theme-med w-full h-1 backdrop-line" />
+</div>
+
+<style>
+  .backdrop-line {
+    opacity: 1;
+  }
+</style>
