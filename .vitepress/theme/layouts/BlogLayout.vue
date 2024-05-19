b8161e5e58f095e45594145e964c0b96737b2030
GeordiD <geordi.dosher@gmail.com>
2024-05-19 16:11:20 -0500

    Get back some of that sexy doc styling

--- .vitepress/theme/layouts/BlogLayout.vue
+++ .vitepress/theme/layouts/BlogLayout.vue
@@ -4,7 +4,7 @@ import BlogHeader from '../components/BlogHeader.vue';
 </script>
 
 <template>
-  <div class="flex flex-col max-w-screen-md gap-4 px-12 py-8 mx-auto mb-16">
+  <div class="flex flex-col max-w-screen-md gap-4 px-12 py-8 mx-auto mb-16 vp-doc">
     <BlogHeader />
     <Content />
   </div>
